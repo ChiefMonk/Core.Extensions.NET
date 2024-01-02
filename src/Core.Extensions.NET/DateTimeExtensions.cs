b4f2d1672edcaad6f3c3ddcf289fb143064baefa
Chief Monk <chipo@outlook.com>
2024-01-02 22:38:15 +0200

    Update project file

--- src/Core.Extensions.NET/DateTimeExtensions.cs
+++ src/Core.Extensions.NET/DateTimeExtensions.cs
@@ -2,5 +2,23 @@
 
 public static class DateTimeExtensions
 {
+	/// <summary>
+	/// Retrieves the first day of the month of the <paramref name="date"/>.
+	/// </summary>
+	/// <param name="date">A date from the month we want to get the first day.</param>
+	/// <returns>A DateTime representing the first day of the month.</returns>
+	public static DateTime FirstDayOfTheMonth(this DateTime date)
+	{
+		return new DateTime(date.Year, date.Month, 1);
+	}
 
+	/// <summary>
+	/// Retrieves the last day of the month of the <paramref name="date"/>.
+	/// </summary>
+	/// <param name="date">A date from the month we want to get the last day.</param>
+	/// <returns>A DateTime representing the last day of the month.</returns>
+	public static DateTime LastDayOfTheMonth(this DateTime date)
+	{
+		return new DateTime(date.Year, date.Month, DateTime.DaysInMonth(date.Year, date.Month));
+	}
 }
---
+++ tests/Core.Extensions.NET.Test/DateTimeExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace Core.Extensions.NET.Test;
+
+public class DateTimeExtensionsTests
+{
+
+	[Fact]
+	public void Get_FirstDayOfTheMonth_For_2023_10_Success()
+	{
+		DateTime date = new DateTime(2023, 10, 23);
+		var responseDate = date.FirstDayOfTheMonth();
+
+		Assert.Equal(new DateTime(2023, 10, 1), responseDate);
+	}
+
+	[Fact]
+	public void Get_FirstDayOfTheMonth_For_2023_10_Fail()
+	{
+		DateTime date = new DateTime(2023, 10, 26);
+		var responseDate = date.FirstDayOfTheMonth();
+
+		Assert.NotEqual(new DateTime(2023, 10, 26), responseDate);
+		Assert.Equal(new DateTime(2023, 10, 1), responseDate);
+	}
+}
+
