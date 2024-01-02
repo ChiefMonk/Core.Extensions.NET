ebe212822168845240e0ba2bc9aef6d8cbab90d7
Chief Monk <chipo@outlook.com>
2024-01-02 22:57:27 +0200

    Updated test cases

--- tests/Core.Extensions.NET.Test/DateTimeExtensionsTests.cs
+++ tests/Core.Extensions.NET.Test/DateTimeExtensionsTests.cs
@@ -1,27 +1,28 @@
-﻿using Xunit;
+﻿using NUnit.Framework;
 
 namespace Core.Extensions.NET.Test;
 
+[TestFixture]
 public class DateTimeExtensionsTests
 {
 
-	[Fact]
+	[Test]
 	public void Get_FirstDayOfTheMonth_For_2023_10_Success()
 	{
 		DateTime date = new DateTime(2023, 10, 23);
 		var responseDate = date.FirstDayOfTheMonth();
 
-		Assert.Equal(new DateTime(2023, 10, 1), responseDate);
+		Assert.That(responseDate, Is.EqualTo(new DateTime(2023, 10, 1)));
 	}
 
-	[Fact]
+	[Test]
 	public void Get_FirstDayOfTheMonth_For_2023_10_Fail()
 	{
 		DateTime date = new DateTime(2023, 10, 26);
 		var responseDate = date.FirstDayOfTheMonth();
 
-		Assert.NotEqual(new DateTime(2023, 10, 26), responseDate);
-		Assert.Equal(new DateTime(2023, 10, 1), responseDate);
+		Assert.That(new DateTime(2023, 10, 26), Is.Not.EqualTo(responseDate));
+		Assert.That(responseDate, Is.EqualTo(new DateTime(2023, 10, 1)));
 	}
 }
 
